4d609a1e256ffeff529f6b0192412a7af4ca8be4
Durga Nirmal <durganirmal123@gmail.com>
2021-11-24 08:49:47 +0530

    Added : Find maximum integer number

--- src/com/Generic_max/Maximum.java
+++ src/com/Generic_max/Maximum.java
@@ -1,39 +1,21 @@
 package com.Generic_max;
 
-public class Maximum<T extends Comparable>{
-T var1, var2, var3;
-
-public Maximum(T var1, T var2, T var3) {
-    this.var1 = var1;
-    this.var2 = var2;
-    this.var3 = var3;
-}
-
+public class Maximum{
 public static void main(String[] args) {
     System.out.println("Welcome to Generic Program.");
     Integer a = 2, b = 4, c =6;
-    Float x =3.4f, y = 4.5f, z = 6.7f;
-    String l="apple",m="banana",n="orange";
-    Maximum<Integer> maximum = new Maximum<>(a, b, c);
-    maximum.max();
-    new Maximum<>(x, y, z).max();
-    new Maximum<>(l, m, n).max();
+    FindMaximum (a,b,c);
 }
 
-private void max() {
-    Maximum.getMaximum(var1,var2,var3);
-}
-
-private static<T extends Comparable> void getMaximum(T a, T b, T c ){
-    T max = a;
+private static void FindMaximum(Integer a, Integer b, Integer c) {
+    Integer max = a;
     if(b.compareTo(a)>0){
         max = b;
     }
     if(c.compareTo(max)>0){
         max = c;
     }
-    System.out.println("Maximum::"+max); max = a;
-
+    System.out.println("Maximum Number is::"+max);
 }
 
 }
